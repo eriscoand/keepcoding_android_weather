ee20b0e294b3d7cd591b5057257779c0f5b0f387
Juanjo <jramirez@agbotraining.com>
2017-05-22 21:49:44 +0200

    New image. One way to attend button clicks using OnClickListener onClick method

--- app/src/main/java/io/keepcoding/guedr/MainActivity.java
+++ app/src/main/java/io/keepcoding/guedr/MainActivity.java
@@ -3,16 +3,51 @@ package io.keepcoding.guedr;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     protected static String TAG = MainActivity.class.getCanonicalName();
+
+    protected Button changeToStone;
+    protected Button changeToDonkey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        changeToStone = (Button) findViewById(R.id.change_stone_system);
+        changeToStone.setOnClickListener(this);
+
+        changeToDonkey = (Button) findViewById(R.id.change_donkey_system);
+        changeToDonkey.setOnClickListener(this);
+
         Log.v(TAG, "Hola Amundio, he pasado por onCreate");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.change_stone_system:
+                Log.v(TAG, "Me han pedido piedra");
+                break;
+            case R.id.change_donkey_system:
+                Log.v(TAG, "Me han pedido burro");
+                break;
+            default:
+                Log.v(TAG, "No sé qué me han pedido");
+        }
+
+//        if (v.getId() == R.id.change_stone_system) {
+//            Log.v(TAG, "Me han pedido piedra");
+//        }
+//        else if (v.getId() == R.id.change_donkey_system) {
+//            Log.v(TAG, "Me han pedido burro");
+//        }
+//        else {
+//            Log.v(TAG, "No sé qué me han pedido");
+//        }
     }
 
     @Override
@@ -22,4 +57,6 @@ public class MainActivity extends AppCompatActivity {
         Log.v(TAG, "Nos han llamado a onSaveInstanceState");
         outState.putString("clave", "valor");
     }
+
+
 }
